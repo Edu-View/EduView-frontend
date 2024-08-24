8f576990ff2c800b9ed8c64bec8e2422b435a9cd
Soe Yan Naing <sync14320@gmail.com>
2024-08-24 19:48:33 +0630

    add axios url

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <AuthProvider>
-      <Routes>
-        <Route path='/*' element={<App/>}/>
-      </Routes>
-    </AuthProvider>
+      <AuthProvider>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
